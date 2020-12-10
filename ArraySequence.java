d5c7bf77e3305d49c91dea04ccdb4bbdd795c55a
alexConkey <aconkey20@stuy.edu>
2020-12-10 09:09:11 -0500

    first two methods done

--- ArraySequence.java
+++ ArraySequence.java
@@ -10,10 +10,17 @@ public class ArraySequence implements IntegerSequence{
   }
   public ArraySequence(IntegerSequence otherseq){
     data = new int[otherseq.length()];
+    otherseq.reset();
     for (int i = 0; i < otherseq.length(); i++){
       data[i] = otherseq.next();
     }
     otherseq.reset();
     currentIndex = 0;
+  }
+  public void reset(){
+    current = data[0];
+  }
+  public int length(){
+    return data.length();
   }
 }
